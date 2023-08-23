5993e22bdffd787ad8141e0c46cef175305b1674
Santos-Satos <Santos-Satos@outlook.jp>
2023-08-23 20:28:47 +0900

    成長度をグローバル変数に変更

--- src/pages/character/index.jsx
+++ src/pages/character/index.jsx
@@ -3,9 +3,10 @@ import { Progress, Space } from 'antd';
 import { useRouter } from "next/router";
 import { styled } from "styled-components";
 import Header from '@/layout/header/components/Header';
-import useGetWindowSize from './GetWindowSize';
+import useGetWindowSize from '../..//features/character/GetWindowSize';
 import { useAtom } from 'jotai';
 import { pointAtom } from '../../features/common/atom';
+import { growthAtom } from '../../features/character/atom';
 
 
 // ボタンのスタイル1
@@ -58,60 +59,61 @@ const Button2 = styled.div`
 // キャラ育成画面を表示するプログラム
 const CharacterGrowing = () => {
     const router = useRouter() // 使用するルータ
-    const [percent, setPercent] = useState(0); // プログレスバーの進捗変数
     const {height, width} = useGetWindowSize(); // 画面のサイズ
-    const [points, setPoints] = useAtom(pointAtom);
-    const use_point = 10;
-    var plus_growth = 1; // 1プッシュで成長するパーセント
+    const [points, setPoints] = useAtom(pointAtom); // 所持ポイント
+    const use_point = 10; // 1プッシュで消費するポイント
+    var plus_growth = 20; // 1プッシュで成長するパーセント
+    const [growth, setGrowth] = useAtom(growthAtom ) // 成長度
 
     // 戻るボタンを押した時に前画面に戻る関数
     const handleExit = () => {
       router.push('/');
     }
 
-    // プログレスバーを増加させる関数
+    // 育てるボタンが押された時に呼び出される関数
     const increase = () => {
-      setPercent((prevPercent) => {
-
+      setGrowth((prevGrowth) => {
+        // ポイントの消費の処理
         if (points >= use_point){
           setPoints(prevPoints => prevPoints - use_point);
         }else{
           plus_growth = 0;
         }
 
-        var newPercent = prevPercent + plus_growth;
-        if (newPercent > 100) newPercent = 100;
+        // 成長度増加の処理
+        var newGrowth = prevGrowth + plus_growth;
+        if (newGrowth > 100) newGrowth = 100;
 
-        // 100%以上になったときの処理
-        if (newPercent == 100){
+        // 成長度が100%以上になったときの処理
+        if (newGrowth == 100){
           Div_PoinGet.style.visibility = "visible";
           Div_Grow.style.visibility = "hidden";
         }
-
-        return newPercent;
+        return newGrowth;
       });
     };
 
     // ポイントの交換をする関数
     const change_point = () => {
-      setPercent((prevPercent) => {
-        var newPercent = 0;
+      setGrowth((prevGrowth) => {
+        var newGrowth = 0;
         Div_PoinGet.style.visibility = "hidden";
         Div_Grow.style.visibility = "visible";
-        return newPercent;
+        return newGrowth;
       });
     };
 
     return (
         <>
             <Header title="マイキャラ育成" onExit={handleExit} />
+            <p> {growth} </p>
             <p> {points} </p>
             <div style={{ background: "white", position: "relative", left:"15px", top:"10px", width: width-50, height: 50}}>
               <p> 成長度 </p>
-              <Progress id="progress" percent={percent} status="active" strokeColor={{ from: '#108ee9', to: '#87d068' }} />
+              <Progress id="progress" percent={growth} status="active" strokeColor={{ from: '#108ee9', to: '#87d068' }} />
             </div>
             <div id="Div_Grow" align="center" style={{ background: "silver", position: "absolute", left: 15, top: height-200, width: width-50, height: 50, visibility: "visible"}}>
-              <Button1 id="Btn_Grow" onClick={increase}>育てる（10 ポイント消費）</Button1>
+              <Button1 id="Btn_Grow" onClick={increase}>育てる（{use_point} ポイント⇒{plus_growth}%成長）</Button1>
             </div>
             <div id="Div_PoinGet" align="center" style={{ background: "silver", position: "absolute", left: 15, top: height-200, width: width-50, height: 50, visibility: "hidden"}} >
               <Button2 onClick={change_point}>成長度100%でポイントゲット！</Button2>
