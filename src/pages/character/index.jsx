80f6ff50ab34f60cd87254e31178e77c3872a36d
Santos-Satos <Santos-Satos@outlook.jp>
2023-08-24 01:07:57 +0900

    キャラ移動機能を追加

--- src/pages/character/index.jsx
+++ src/pages/character/index.jsx
@@ -1,4 +1,6 @@
+
 import { Progress, Space } from 'antd';
+import { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
 import { styled } from "styled-components";
 import Header from '@/layout/header/components/Header';
@@ -54,6 +56,17 @@ const Button2 = styled.div`
   }
 `;
 
+// キャラ移動の方向を決める関数
+const getRandomDirection = () => {
+  const angle = Math.random() * Math.PI * 2;
+  return { x: Math.cos(angle), y: Math.sin(angle) };
+};
+
+// キャラ移動の速度を決める関数
+const getRandomSpeed = () => {
+  return Math.random() * 2 + 1;
+};
+
 
 // キャラ育成画面を表示するプログラム
 const CharacterGrowing = () => {
@@ -62,7 +75,17 @@ const CharacterGrowing = () => {
     const [points, setPoints] = useAtom(pointAtom); // 所持ポイント
     const use_point = 10; // 1プッシュで消費するポイント
     var plus_growth = 10; // 1プッシュで成長するパーセント
-    const [growth, setGrowth] = useAtom(growthAtom ) // 成長度
+    const [growth, setGrowth] = useAtom(growthAtom); // 成長度
+    const x1 = 50; // キャラの移動域のx座標（左上）
+    const y1 = 200; // キャラの移動域のy座標（左上）
+    const x2 = width-100; // キャラの移動域のx座標（右下）
+    const y2 = height-100; // キャラの移動域のy座標（右下）
+    const minStopDuration = 1000; // キャラの最短静止時間 [msec]
+    const maxStopDuration = 3000; // キャラの最長静止時間 [msec]
+    const [position, setPosition] = useState({ x: 200, y: 300 });　// キャラの初期位置
+    const [direction, setDirection] = useState(getRandomDirection()); //キャラ移動の方向を表す
+    const [speed, setSpeed] = useState(getRandomSpeed()); //キャラ移動の速度を表す
+    const [isChangingDirection, setIsChangingDirection] = useState(false); // キャラ移動の方向転換を表す
 
     // 戻るボタンを押した時に前画面に戻る関数
     const handleExit = () => {
@@ -102,13 +125,54 @@ const CharacterGrowing = () => {
       });
     };
 
+    // キャラ移動を処理する関数
+    useEffect(() => {
+      const updatePosition = () => {
+        // 方向転換の処理
+        if (isChangingDirection) {
+          const stopDuration = Math.random() * (maxStopDuration - minStopDuration) + minStopDuration;
+          setTimeout(() => {
+            setDirection(getRandomDirection());
+            setSpeed(getRandomSpeed());
+            setIsChangingDirection(false);
+          }, stopDuration);
+        }
+
+        //  移動座標の計算
+        const newPosition = {
+          x: position.x + direction.x * speed,
+          y: position.y + direction.y * speed,
+        };
+        if (
+          newPosition.x < x1 ||
+          newPosition.x > x2 ||
+          newPosition.y < y1 ||
+          newPosition.y > y2
+        ) {
+          setIsChangingDirection(true);
+        } else {
+          setPosition(newPosition);
+        }
+      };
+      const animationFrame = requestAnimationFrame(updatePosition);
+      return () => cancelAnimationFrame(animationFrame);
+    }, [position, direction, speed, isChangingDirection]);
+
     // 戻り値
     return (
       <div>
         <div align="center" style={{position: "absolute", left:0, top:0, width:width-10}}>
           <img src="./img_bg.png" style={{width: width, height: height}}/>
         </div>
-
+        <img
+        src="/path/to/your/image.png" // Change this to the path of your image
+        alt="Moving Image"
+        style={{
+          position: 'absolute',
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+        }}
+      />
         <div style={{background: "rgba(255,255,255,0.8)", position: "absolute", left:10, top:100, width: 300, height: 50}}>
             <p> 成長度 </p>
             <Progress id="progress" percent={growth} status="active" strokeColor={{ from: '#108ee9', to: '#87d068' }} />
@@ -128,6 +192,7 @@ const CharacterGrowing = () => {
             <p> 交換レート：{use_point} ポイント⇒{plus_growth}%成長</p>
           </div>
         </div>
+
         <Header title="マイキャラ育成" onExit={handleExit}/>
       </div>
     );
